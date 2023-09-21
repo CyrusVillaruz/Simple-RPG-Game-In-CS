fe0ac9fefc2c85eaeacbf8e1407982cf4804713b
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2023-09-21 11:39:20 -0700

    Implements a Player Class

--- Simple RPG/SimpleRPG.cs
+++ Simple RPG/SimpleRPG.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,24 +14,20 @@ namespace Simple_RPG
 {
     public partial class SimpleRPG : Form
     {
+        private Player _player;
         public SimpleRPG()
         {
             InitializeComponent();
-        }
-
-        private void label8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lblGold.Text = "123";
+            _player = new Player();
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
     }
 }
